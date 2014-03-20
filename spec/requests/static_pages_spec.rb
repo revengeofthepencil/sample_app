229f45b5113fc3a50dca0a05a91ab35745d28454
Alex Walker <ahwalker@uw.edu>
2014-03-20 15:49:38 -0700

    ch 5 exercise 1

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,11 +2,10 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
-  
+  subject { page }
+
   describe "Home page" do
     before { visit root_path }
-    subject { page }
     it { should have_content('Sample App') }
     it { should have_title(full_title('')) }
     it { should_not have_title('| Home') }
@@ -14,23 +13,21 @@ describe "Static pages" do
   
   describe "Help page" do
     before { visit help_path }
-    subject { page }
     it { should have_title(full_title('Help')) }
-    it { should have_title("#{base_title} | Help") }
+    it { should have_title(full_title('Help')) }
   end 
 
   describe "About page" do
     before { visit about_path }
-    subject { page }
     it { should have_content('About Us') }
     it { should have_title(full_title('About')) }
   end
 
   describe "Contact page" do
     before { visit contact_path }
-    subject { page }
-    it { should have_content('Contact') }
-    it { should have_title(full_title('Contact')) }
+    it { should have_selector('h1', text: 'Contact') }
+    it { should have_title(full_title('Contact')) }    
+    
   end  
     
 end
