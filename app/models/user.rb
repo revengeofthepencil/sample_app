0faf33b1e75f38706ba3b4493fd49ef1fb0de861
Alex Walker <ahwalker@uw.edu>
2014-03-22 16:25:39 -0700

    updates to user spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
     uniqueness: {case_sensitive: false}
   validates :password, length: { minimum: 6 }
-  before_save { self.email = email.downcase }
-
+  before_save { email.downcase! }
+  
   has_secure_password
 
 end
